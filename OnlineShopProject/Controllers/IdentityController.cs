f138950a25e466585648072c178d444cc86049ba
shelk <shelk@ITZSASHA>
2023-11-29 17:29:38 +0300

    Страницы входа/регистраций

---
+++ OnlineShopProject/Controllers/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopProject.Controllers
+{
+    public class IdentityController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
